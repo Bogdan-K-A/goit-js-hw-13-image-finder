28518461fa3c9dbc1aeb94b35d5e32c07351950c
Bogdan-K-A <kozhoma_b@ukr.net>
2021-10-23 22:12:32 +0300

    дз13

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/modern-normalize.css';
+import './css/style.css';
+
+import './js/add.js';
+import './js/apiService.js';
---
+++ src/js/apiService.js
@@ -0,0 +1,15 @@
+// const API_KEY = '23996907-65b7baf3ba7389d44636c5d9e'
+// const query = 'car'
+// const numPage = 1
+// const BASE_URL = `https://pixabay.com/api/`
+// const options = `?image_type=photo&orientation=horizontal&q=${query}&page=${numPage}&per_page=12&key=${API_KEY}`
+// const URL = BASE_URL + options
+
+// // export default
+// function fetchPhotos(searchQuery) {
+//   fetch(URL)
+//     .then((response) => {
+//       console.log(response.json())
+//     })
+//     .catch((err) => {})
+// }
